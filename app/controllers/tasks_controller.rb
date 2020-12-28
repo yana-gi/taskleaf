7a04a7e3085c220902711632e03975473dce8cbf
yana-gi <yanagisawa.hika@gmail.com>
2020-12-28 18:22:09 +0900

    p101 newメソッドに@task追加 アクションの役割

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,6 +6,11 @@ class TasksController < ApplicationController
   end
 
   def new
+    # 画面をスムーズに描画するために、新しいtaskオブジェクト生成
+    # アクションからビューに受け渡しをしたいデータをインスタンス変数に入れる
+    # 返す画面を省略した場合、アクション名に対応する場面を返す
+    # 今回の様に省略した場合、 app/views/tasks/new.html.slim が返される
+    @task = Task.new
   end
 
   def edit
