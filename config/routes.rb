16252c9c0525e6f5465af6bf7ab649244fe33b88
yana-gi <yanagisawa.hika@gmail.com>
2020-12-28 18:05:26 +0900

    p98 resourcesメソッド

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/show'
-  get 'tasks/new'
-  get 'tasks/edit'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resourcesメソッドでtaskのアクションを一括で設定することができる
+  resources :tasks
 end
